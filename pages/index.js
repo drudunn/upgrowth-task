d409e17c9d1e0d16db61a937d18e8131bcaf39b6
Andru Dunn <andru@dunn.wtf>
2020-01-31 23:23:50 +1100

    Add Layout and add Search component

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,30 @@
-import React from 'react'
-import styled from 'styled-components'
+import Head from 'next/head';
+import styled from 'styled-components';
+import Search from '../components/search/Search';
+
+const Layout = styled.main`
+  max-width: 1200px;
+  margin: 0 auto;
+  padding: 1rem;
+`;
 
 const Title = styled.h1`
   font-size: 50px;
   color: ${({ theme }) => theme.colors.primary};
-`
+  text-align: center;
+`;
 
-export default () => <Title>My page</Title>
+const Homepage = () => (
+  <>
+    <Head>
+      <title>Unsplash'd - Image search for Upgrowth</title>
+    </Head>
+
+    <Layout>
+      <Title>Unsplash'd</Title>
+      <Search />
+    </Layout>
+  </>
+);
+
+export default Homepage;
