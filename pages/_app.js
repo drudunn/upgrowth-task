3d9b19544987ecef5b8ee5ee387d853811a31441
Andru Dunn <andru@dunn.wtf>
2020-01-31 23:20:33 +1100

    Extract theme into its own file

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,8 @@
 import App from 'next/app'
 import React from 'react'
 import { ThemeProvider } from 'styled-components'
+import theme from '../theme/theme';
 
-const theme = {
-  colors: {
-    primary: '#0070f3',
-  },
-}
 
 export default class MyApp extends App {
   render() {
---
+++ theme/theme.js
@@ -0,0 +1,12 @@
+const theme = {
+  colors: {
+    primary: '#eb4d4b',
+    secondary: '#6ab04c',
+    secondaryDark: '#374a00'
+  },
+  background: {
+    highlight: '#f9ffe8'
+  }
+};
+
+export default theme;
